8c7548a067f7eb3438ea0154c1ec2f7601b8f0da
jackkower <jackkower@gmail.com>
2020-10-20 23:14:48 -0700

    next release

---
+++ aws-test/build/precache-manifest.251e427847e060aa8587352c86149462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b75451a950c45e72356fd056d7d391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8a2e18b2ef172a8686d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edd6b88147bce2a6c7a3",
+    "url": "/static/js/2.0e2fcea7.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.0e2fcea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a2e18b2ef172a8686d",
+    "url": "/static/js/main.05562c15.chunk.js"
+  },
+  {
+    "revision": "28781ce3f7d0307ac783",
+    "url": "/static/js/runtime-main.28d1db3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
